315be196e016d02ebb344e7341bfeabf87837118
01fe22bca162 <01fe22bca162@kletech.ac.in>
2024-02-20 09:48:21 +0530

    Updated code changes to square and cube operations.

---
+++ Extended_Calculator.java
@@ -0,0 +1,14 @@
+/**
+ * The `Extended_Calculator` class provides methods for calculating the square and cube of numbers.
+ */
+
+public class Extended_Calculator{
+    float square(float operand_1){
+        return(operand_1*operand_1);
+    }
+    float cube(float operand_1){
+        return(operand_1*operand_1*operand_1);
+    }
+    
+   
+}
